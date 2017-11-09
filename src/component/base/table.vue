ed5453cfb144cc63c4f89af1401eb1feb6d4c989
cheng87126 <cheng87126@gmail.com>
2017-11-09 23:08:46 +0800

    add table.vue

---
+++ src/component/base/table.vue
@@ -0,0 +1,116 @@
+<style scoped>
+tbody td img{
+	width: 1.2em;
+	vertical-align: middle;
+	cursor: pointer;
+}
+.item-row input[type="text"]{
+	display: none;
+}
+.edit span{
+	display: none;
+}  
+.edit input[type="text"]{
+	display: block;
+	width: 100%;
+	height: 100%;
+	box-sizing: border-box;
+}
+</style>
+<template>
+    <table>
+	    <colgroup>
+	    	<col width="5%">
+	    	<col width="50%">
+	    	<col width="35%">
+	    	<col width="10%">
+	    </colgroup>
+		<thead>
+			<tr>
+				<th><input type="checkbox" name="all" id="all"></th>
+				<th>地址</th>
+				<th>描述</th>
+				<th>操作</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr class="item-row"
+				v-for="item in urls" :key="item.id" 
+				v-bind:class="{edit:isEdit === item.id}">
+				<td><input type="checkbox" name="" id=""></td>
+				<td><a v-bind:href="item.urlName">{{ item.urlName }}</a></td>
+				<td v-on:dblclick="edit(item.id,item.urlDesc)">
+					<span>{{ item.urlDesc }}</span>
+					<input type="text" 
+						v-model="item.urlDesc"
+						v-focus="isEdit === item.id"
+						v-on:keyup.esc="cancel(item)"
+						v-on:blur="update(item.urlDesc,item.id)"
+						v-on:keyup.enter="update(item.urlDesc,item.id)"
+					>
+				</td>
+				<td><img v-on:click="delItem(item.id)" src="../../img/del.svg" alt="del"></td>
+			</tr>
+		</tbody>
+	</table>
+</template>
+<script>
+const AV = require('leancloud-storage')
+
+export default {
+    name:'Table',
+    data(){
+        return {
+			isEdit:''
+        }
+    },
+    props:{
+        urls:Array
+    },
+    methods:{
+		delItem(id){
+			let link = AV.Object.createWithoutData('link',id)
+			link.destroy().then(success=>{
+				this.$store.commit('setToast',{
+					show:true,
+					msg:'删除成功!'
+				})
+                // this.getList()
+                this.$emit('refreshList')
+			},error=>{
+				// 删除失败
+			})
+		},
+		edit(id,desc){
+			this.isEdit = id
+			//避免修改内容后按esc取消修改不能还原desc
+			this.descCache = desc
+		},
+		update(desc,id){
+			if(!this.isEdit){
+				return
+			}
+			AV.Query.doCloudQuery(`update link set url_desc="${desc}" where objectId="${id}"`)
+			.then(data => {
+				// data 中的 results 是本次查询返回的结果，AV.Object 实例列表
+				var results = data.results
+				this.isEdit = ''
+			}, error => {
+				// 异常处理
+				console.error(error)
+			});
+		},
+		cancel(item){
+			this.isEdit = ''
+			item.urlDesc = this.descCache
+		}
+    },
+	directives:{
+		focus:function(el,binding){
+			if (binding.value) {
+				el.focus()
+			}
+		}
+	}
+}
+</script>
--- src/component/url.vue
+++ src/component/url.vue
@@ -1,21 +1,5 @@
-<style scoped>
-	tbody td img{
-		width: 1.2em;
-		vertical-align: middle;
-		cursor: pointer;
-	}
-	.item-row input[type="text"]{
-		display: none;
-	}
-	.edit span{
-		display: none;
-	}  
-	.edit input[type="text"]{
-		display: block;
-		width: 100%;
-		height: 100%;
-		box-sizing: border-box;
-	}
+<style>
+	
 </style>
 <template>
 	<div>
@@ -26,149 +10,74 @@
 			<label for="desc">描述</label><input type="text" id="desc" v-model="desc">
 		</div>
 		<div class="btn" v-on:click="submitUrl">提交</div>
-		<table>
-			<colgroup>
-				<col width="5%">
-				<col width="50%">
-				<col width="35%">
-				<col width="10%">
-			</colgroup>
-			<thead>
-				<tr>
-					<th><input type="checkbox" name="all" id="all"></th>
-					<th>地址</th>
-					<th>描述</th>
-					<th>操作</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr class="item-row"
-					v-for="item in urlList" :key="item.id" 
-					v-bind:class="{edit:isEdit === item.id}">
-					<td><input type="checkbox" name="" id=""></td>
-					<td><a v-bind:href="item.urlName">{{ item.urlName }}</a></td>
-					<td v-on:dblclick="edit(item.id,item.urlDesc)">
-						<span>{{ item.urlDesc }}</span>
-						<input type="text" 
-							v-model="item.urlDesc"
-							v-focus="isEdit === item.id"
-							v-on:keyup.esc="cancel(item)"
-							v-on:blur="update(item.urlDesc,item.id)"
-							v-on:keyup.enter="update(item.urlDesc,item.id)"
-						>
-					</td>
-					<td><img v-on:click="delItem(item.id)" src="../img/del.svg" alt="del"></td>
-				</tr>
-			</tbody>
-		</table>
+
+		<w-table v-bind:urls="urlList"
+				v-on:refreshList="getList"></w-table>
 	</div>
 </template>
 <script>
-	const AV = require('leancloud-storage')
+const AV = require('leancloud-storage')
+import Table from './base/table.vue'
 
-	export default{
-		name:'Url',
-		data(){
-			return {
-				desc:'',
-				url:'',
-				urlList:[],
-				isEdit:''
-			}
+export default{
+	name:'Url',
+	data(){
+		return {
+			desc:'',
+			url:'',
+			urlList:[]
+		}
+	},
+	components:{
+		'w-table':Table
+	},
+	methods:{
+		submitUrl() {
+			let LinkObj = AV.Object.extend('link')
+			let linkObj = new LinkObj()
+			linkObj.save({
+				url_desc:this.desc,
+				url_name: this.url
+			}).then(object => {
+				this.getList()
+				this.desc = ''
+				this.url = ''
+				this.$store.commit('setToast',{
+					show:true,
+					msg:'添加成功!'
+				})
+			})
 		},
-		methods:{
-			submitUrl() {
-				let LinkObj = AV.Object.extend('link')
-				let linkObj = new LinkObj()
-
-				linkObj.save({
-					url_desc:this.desc,
-					url_name: this.url
-				}).then(object => {
-					this.getList()
-					this.desc = ''
-					this.url = ''
-
-					this.$store.commit('setToast',{
-						show:true,
-						msg:'添加成功!'
+		getList(){
+			let query = new AV.Query('link')
+			query.find().then(result=>{
+				this.urlList = []
+				result.forEach(item => {
+					this.urlList.push({
+						urlDesc:item.get('url_desc'),
+						urlName:item.get('url_name'),
+						id:item.id,
+						createdAt:item.createdAt
 					})
 				})
-			},
-			delItem(id){
-				let link = AV.Object.createWithoutData('link',id)
-				link.destroy().then(success=>{
-					this.$store.commit('setToast',{
-						show:true,
-						msg:'删除成功!'
-					})
-					this.getList()
-				},error=>{
-					// 删除失败
+				this.urlList.sort(function(a,b){
+					return b.createdAt - a.createdAt
 				})
-			},
-			getList(){
-				let query = new AV.Query('link')
-				query.find().then(result=>{
-					this.urlList = []
-					result.forEach(item => {
-						this.urlList.push({
-							urlDesc:item.get('url_desc'),
-							urlName:item.get('url_name'),
-							id:item.id,
-							createdAt:item.createdAt
-						})
-					})
-
-					this.urlList.sort(function(a,b){
-						return b.createdAt - a.createdAt
-					})
-				},error=>{
-					console.log(error)
-				})
-			},
-			edit(id,desc){
-				this.isEdit = id
-				//避免修改内容后按esc取消修改不能还原desc
-				this.descCache = desc
-			},
-			update(desc,id){
-				if(!this.isEdit){
-					return
-				}
-				AV.Query.doCloudQuery(`update link set url_desc="${desc}" where objectId="${id}"`)
-				.then(data => {
-					// data 中的 results 是本次查询返回的结果，AV.Object 实例列表
-					var results = data.results
-					this.isEdit = ''
-				}, error => {
-					// 异常处理
-					console.error(error)
-				});
-			},
-			cancel(item){
-				this.isEdit = ''
-				item.urlDesc = this.descCache
-			}
-		},
-		mounted() {
-			this.getList()
-
-			let currentUser = AV.User.current()
-			if (currentUser) {
-				// 跳转到首页
-			}
-			else {
-				//currentUser 为空时，可打开用户注册界面…
-				this.$router.push({ path: '/login' })
-			}
-		},
-		directives:{
-			focus:function(el,binding){
-				if (binding.value) {
-					el.focus()
-				}
-			}
+			},error=>{
+				console.log(error)
+			})
+		}
+	},
+	mounted() {
+		this.getList()
+		let currentUser = AV.User.current()
+		if (currentUser) {
+			// 跳转到首页
+		}
+		else {
+			//currentUser 为空时，可打开用户注册界面…
+			this.$router.push({ path: '/login' })
 		}
 	}
+}
 </script>
